4354d4d678f37b8674a77e37e96785f0f22495c7
Wojciech Charysz <wojciech.charysz@hmmh.de>
2017-01-20 17:02:04 +0100

    Base iOS project

---
+++ Charysz.Wojciech.appCuisine/Charysz.Wojciech.appCuisineTests/Charysz_Wojciech_appCuisineTests.swift
@@ -0,0 +1,36 @@
+//
+//  Charysz_Wojciech_appCuisineTests.swift
+//  Charysz.Wojciech.appCuisineTests
+//
+//  Created by Wojciech Charysz on 20.01.17.
+//  Copyright © 2017 Wojciech Charysz. All rights reserved.
+//
+
+import XCTest
+@testable import Charysz_Wojciech_appCuisine
+
+class Charysz_Wojciech_appCuisineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
