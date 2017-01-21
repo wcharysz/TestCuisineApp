f3c99f2d63844be24f127de9089f7d056eeb200f
Wojciech Charysz <wojciech.charysz@hmmh.de>
2017-01-21 08:13:52 +0100

    Displaying dates and cameras’ full names

--- Charysz.Wojciech.appCuisine/Charysz.Wojciech.appCuisine/ViewController.swift
+++ Charysz.Wojciech.appCuisine/Charysz.Wojciech.appCuisine/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var photos: [Photo]?
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let api = NetworkController()
+        api.downloadNASARoverPhotos { (photos) in
+            self.photos = photos
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NasaMarsRoverCell", for: indexPath) as? NasaMarsRoverTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let photo = photos?[indexPath.row]
+        cell.cameraFullNameLabel.text = photo?.cameraFullName
+        
+        if let date = photo?.earth_date {
+            cell.earthDateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
