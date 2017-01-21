d431ec94a59438b1af567f97c5093bb2dcb9a039
Wojciech Charysz <wojciech.charysz@hmmh.de>
2017-01-21 01:15:50 +0100

    Cocoapods

---
+++ Charysz.Wojciech.appCuisine/Pods/Target Support Files/Pods-Charysz.Wojciech.appCuisine/Pods-Charysz.Wojciech.appCuisine-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Charysz_Wojciech_appCuisineVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Charysz_Wojciech_appCuisineVersionString[];
+
